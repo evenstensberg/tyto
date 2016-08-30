9dbd732d1186a68d582ff29976f99b3f1e544b3e
evenstensberg <dweaster@live.no>
2016-08-30 11:25:44 +0200

    :cyclone: fix test :cyclone:

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var compare = require('./src/compare')
 
 test('it should compare two values', t => {
    const a = [2,4,6,8,10]
-   const fun = (num) => num*num
+   const fun = (num) => (num*num)
    const count = compare(a,fun)
 
    t.deepEqual(count[0], [2,4,6,8,10]);
