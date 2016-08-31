099d9d3cb154b5c30a42514f33805a6136978666
evenstensberg <dweaster@live.no>
2016-08-31 18:56:11 +0200

    new modules

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const compare = require('./lib/compare');
 const flap = require('./lib/flap');
 const fly = require('./lib/fly');
+const hunt = require('./lib/hunt');
+
 exports.compare = compare;
 exports.flap = flap;
 exports.fly = fly;
+exports.hunt = hunt;
