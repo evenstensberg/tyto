0f1aefac75c30fc26531d3bad57939eb94ac03df
evenstensberg <dweaster@live.no>
2016-09-02 19:43:22 +0200

    Applied comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const compare = require('./main').compare
 const repl = require('repl');
 
+// needs to output a customized, function based by user input
 repl.start({prompt: '>', eval: myEval, writer: myWriter});
 
 function myEval(cmd, context, filename, callback) {
